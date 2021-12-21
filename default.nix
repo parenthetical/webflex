8fa62c6939f2afdd5e3a67add076f5e2aa5aa9ed
Adriaan Leijnse <parenthetical@users.noreply.github.com>
2021-12-21 11:35:18 +0000

    Haskell language server.

--- default.nix
+++ default.nix
@@ -8,13 +8,22 @@ in (import sources.reflex-platform { inherit system; }).project ({ pkgs, ... }: 
     reflex-extras = ./reflex-extras;
   };
   overrides = self: super: {
-    # See: https://github.com/reflex-frp/reflex-platform/issues/717
-    # mmorph = self.callHackage "mmorph" "1.1.3" {};
+    data-tree-print = self.callHackage "data-tree-print" "0.1.0.2" {};
+    ghc-exactprint = self.callHackage "ghc-exactprint" "0.6.3.4" {};
+    stylish-haskell = self.callHackage "stylish-haskell" "0.12.2.0" {};
+    # ghcide = self.callHackage "ghcide" "1.5.0.1" {};
+    brittany = self.callHackage "brittany" "0.13.1.2" {};
+    # implicit-hie = self.callHackage "implicit-hie" "0.1.2.5" {};
   };
 
-  shellToolOverrides = self: super: {
-    inherit (pkgs.haskell.packages.ghc865) haskell-language-server;
-    inherit (pkgs.haskell.packages.ghc865) retrie;
+  # shellToolOverrides = self: super: {
+  #   inherit (pkgs.haskell.packages.ghc8104) haskell-language-server;
+  #   inherit (pkgs.haskell.packages.ghc8104) retrie;
+  # };
+  shellToolOverrides = ghc: super: {
+    inherit (ghc) haskell-language-server;
+    # inherit (ghc) hie-bios;
+    # inherit (ghc) implicit-hie;
   };
 
   useWarp = true;
@@ -23,6 +32,6 @@ in (import sources.reflex-platform { inherit system; }).project ({ pkgs, ... }: 
   
   shells = {
     ghc8_10 = [ "webflex-core" "webflex-sim" "reflex-extras" ];
-    ghcjs = [ "webflex-core" "webflex-sim" ];
+    ghcjs8_10 = [ "webflex-core" "webflex-sim" ];
   };
 })
